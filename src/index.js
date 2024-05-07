495e06c8af36009154164f40a358e592d6e92439
isac7722 <isac7722@gmail.com>
2024-05-07 15:26:50 +0900

    zz cxcx

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { LetterPage } from './letterPage/LetterPage';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -9,12 +10,10 @@ root.render(
   <BrowserRouter>
   <Routes>
     <Route path="/" element={<App />} />
+    <Route path="/LetterPage" element={<LetterPage/>}/>
   </Routes>
   </BrowserRouter>
   
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 
