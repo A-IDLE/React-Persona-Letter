64c0c85a666cbaa9f842a341dd2525a3b90feb4f
isac7722 <isac7722@gmail.com>
2024-05-07 21:32:37 +0900

    main page fixed

--- src/Main.js
+++ src/Main.js
@@ -3,61 +3,37 @@ function App() {
     <div className="series-container">
       <h2>Harry Potter series</h2>
       <div className="character-row">
-        <div className="character">
-          <img src="/images/Harry_Potter.jpg" alt="Harry Potter" />
-          <p>Harry Potter</p>
-        </div>
-        <div className="character">
-          <img src="/images/Hermione_Granger.jpg" alt="Hermione Granger" />
-          <p>Hermione Granger</p>
-        </div>
-        <div className="character">
-          <img src="/images/Ron_Weasley.jpg" alt="Ron Weasley" />
-          <p>Ron Weasley</p>
-        </div>
-        <div className="character">
-          <img src="/images/Ginny_Weasley.jpg" alt="Ginny Weasley" />
-          <p>Ginny Weasley</p>
-        </div>
-        <div className="character">
-          <img src="/images/Severus_Snape.jpg" alt="Severus Snape" />
-          <p>Severus Snape</p>
-        </div>
-        <div className="character">
-          <img src="/images/Luna_Lovegood.jpg" alt="Luna Lovegood" />
-          <p>Luna Lovegood</p>
-        </div>
+        <CharacterButton characterName="Harry Potter" />
+        <CharacterButton characterName="Hermione Granger" />
+        <CharacterButton characterName="Ron Weasley" />
+        <CharacterButton characterName="Ginny Weasley" />
+        <CharacterButton characterName="Severus Snape" />
+        <CharacterButton characterName="Luna Lovegood" />
       </div>
 
       <h2>The Walking Dead</h2>
       <div className="character-row">
-        <div className="character">
-            <img src="/images/Glenn_Rhee.jpg" alt="Glenn Rhee" />
-            <p>Glenn Rhee</p>
-        </div>
-        <div className="character">
-          <img src="/images/Daryl_Dixon.jpg" alt="Daryl Dixon" />
-          <p>Daryl Dixon</p>
-        </div>
-        <div className="character">
-          <img src="/images/Michonne.jpg" alt="Michonne" />
-          <p>Michonne</p>
-        </div>
-        <div className="character">
-          <img src="/images/Carl_Grimes.jpg" alt="Carl Grimes" />
-          <p>Carl Grimes</p>
-        </div>
-        <div className="character">
-          <img src="/images/Carol_Peletier.jpg" alt="Carol Peletier" />
-          <p>Carol Peletier</p>
-        </div>
-        <div className="character">
-          <img src="/images/Maggie_Greene.jpg" alt="Maggie Greene" />
-          <p>Maggie Greene</p>
-        </div>
+      <CharacterButton characterName="Glenn Rhee" />
+      <CharacterButton characterName="Daryl Dixon" />
+      <CharacterButton characterName="Michonne" />
+      <CharacterButton characterName="Carl Grimes" />
+      <CharacterButton characterName="Carol Peletier" />
+      <CharacterButton characterName="Maggie Greene" />
       </div>
     </div>
   );
 }
 
 export default App;
+
+const CharacterButton = ({ characterName }) => {
+
+  const imageUrl = `/images/${characterName}.jpg`; // Construct the image URL
+
+  return (
+    <div className="character">
+      <img src={imageUrl} alt={characterName} />
+      <p>{characterName}</p>
+    </div>
+  );
+}
