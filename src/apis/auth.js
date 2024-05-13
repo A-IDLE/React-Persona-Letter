c9fc90bbb871ad3cd8a7cdee2a00167522436ab0
isac7722 <isac7722@gmail.com>
2024-05-13 16:41:01 +0900

    useAuth improvements

--- src/apis/auth.js
+++ src/apis/auth.js
@@ -1,7 +1,14 @@
-import {request} from "../apis/api"
+import { request } from "../apis/api"
 
-  // Google Login api
-  export const googleLogin = async (accessToken) => {
-    const response = await request.post("/googleLogin", {accessToken});
-    return response.data;
-      }
+// Google Login api
+export const googleLogin = async (accessToken) => {
+  const response = await request.post("/googleLogin", { accessToken });
+  return response.data;
+}
+
+
+// accesstoken validation api
+export const validateToken = async (accessToken) => {
+  const response = await request.post("/validateToken", { accessToken });
+  return response.data;
+}
--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,4 +1,26 @@
-export function useAuth() {
+import { validateToken } from "../apis/auth";
+
+// export function useAuth() {
+//     const accessToken = localStorage.getItem('accessToken');
+//     return !!accessToken;
+//   }
+
+
+  export function useAuth() {
     const accessToken = localStorage.getItem('accessToken');
-    return !!accessToken;
+  
+    if (accessToken) {
+      try {
+        // 먼저 해당 토큰이 유효한지 확인
+        const response = validateToken(accessToken);
+        return true;
+      } catch (error) {
+        console.error("Error validating token:", error);
+        // 토큰이 유효하지 않으면 localStorage에서 삭제
+        localStorage.removeItem('accessToken');
+        return false;
+      }
+    }
+    // Return false if there is no accessToken
+    return false;
   }
