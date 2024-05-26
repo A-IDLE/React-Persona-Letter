5205e6ea46c14487044bf9c15bb71933235d0992
hsh30835 <login7h@gmail.com>
2024-05-26 18:52:36 +0900

    리셋버튼 컴포넌트

--- src/pages/letterPage/LetterPageWrapper.js
+++ src/pages/letterPage/LetterPageWrapper.js
@@ -33,3 +33,33 @@
 // };
 
 // export default LetterPageWrapper;
+
+
+
+
+
+  // useEffect(() => {
+  //   const images = document.querySelectorAll('.fly img');
+  //   let currentImageIndex = 0;
+
+  //   const showNextImage = () => {
+  //     images.forEach(img => img.classList.add('hidden')); // 모든 이미지를 숨김
+  //     images[currentImageIndex].classList.remove('hidden'); // 현재 이미지를 표시
+  //     currentImageIndex = (currentImageIndex + 1) % images.length;
+  //   };
+
+  //   // 처음에 모든 이미지를 숨기고, 순차적으로 나타나게 함
+  //   images.forEach(img => img.classList.add('hidden'));
+  //   setTimeout(() => {
+  //     showNextImage();
+  //     const intervalId = setInterval(showNextImage, 1000); // 1초 간격으로 이미지 변경
+  //     return () => clearInterval(intervalId);
+  //   }, 0); // 처음 1초 후 첫 이미지 나타남
+  // }, []);
+
+
+  /* <div className='fly'>
+        <img src="/images/letterPage/1.png" alt="1" />
+        <img src="/images/letterPage/2.png" alt="2" />
+        <img src="/images/letterPage/.png" alt="3" />
+      </div> */
--- src/pages/sendLetter/Sending.js
+++ src/pages/sendLetter/Sending.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
-import { useLocation, useNavigate, } from 'react-router-dom';
-import "./Sending.css";
+import { useLocation, useNavigate } from 'react-router-dom';
+import './Sending.css';
 
 const Sending = () => {
   const navigate = useNavigate();
@@ -9,44 +9,17 @@ const Sending = () => {
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      navigate('/', {state: {characterId}}); // 이동할 경로로 변경
-    }, 3000); // 3초 뒤에 이동
+      navigate('/', { state: { characterId } }); // 이동할 경로로 변경
+    }, 2000); // 3초 뒤에 이동
 
     return () => clearTimeout(timer);
-  }, [navigate]);
-
-  // useEffect(() => {
-  //   const images = document.querySelectorAll('.fly img');
-  //   let currentImageIndex = 0;
-
-  //   const showNextImage = () => {
-  //     images.forEach(img => img.classList.add('hidden')); // 모든 이미지를 숨김
-  //     images[currentImageIndex].classList.remove('hidden'); // 현재 이미지를 표시
-  //     currentImageIndex = (currentImageIndex + 1) % images.length;
-  //   };
-
-  //   // 처음에 모든 이미지를 숨기고, 순차적으로 나타나게 함
-  //   images.forEach(img => img.classList.add('hidden'));
-  //   setTimeout(() => {
-  //     showNextImage();
-  //     const intervalId = setInterval(showNextImage, 1000); // 1초 간격으로 이미지 변경
-  //     return () => clearInterval(intervalId);
-  //   }, 0); // 처음 1초 후 첫 이미지 나타남
-  // }, []);
+  }, [navigate, characterId]);
 
   return (
     <>
-    <div>
-      {/* <div className='fly'>
-        <img src="/images/letterPage/1.png" alt="1" />
-        <img src="/images/letterPage/2.png" alt="2" />
-        <img src="/images/letterPage/3.png" alt="3" />
-      </div> */}
-      <img src="/images/letterPage/letter.gif" alt="부엉이" className="owl" />
-    </div>
-    <div>
-      <p>전송중</p>
-    </div>
+      <div>
+        <img src="/images/sendLetter/send_letter.png" alt="편지" className="owl" />
+      </div>
     </>
   );
 };
