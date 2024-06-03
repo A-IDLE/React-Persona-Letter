08329995da7d22afc5fc3408e607d8139ec8bba7
hsh30835 <login7h@gmail.com>
2024-06-03 12:29:50 +0900

    화면수정

--- src/components/buttons/ResetButton.js
+++ src/components/buttons/ResetButton.js
@@ -1,23 +1,23 @@
-// 리셋 버튼 컴포넌트
 import { deleteLetters } from "../../apis/letterApi";
+import { useNavigate } from "react-router-dom";
 import "./ResetButton.css"
 
-export const ResetButton = ({characterId }) => {
-
-    
+export const ResetButton = ({ characterId, onReset }) => {
+    const navigate = useNavigate();
 
     const handleReset = async () => {
         console.log("리셋");
         const userConfirmed = window.confirm("캐릭터와의 기억과 편지 내용을 모두 제거하는 버튼입니다. 하시겠습니까?");
 
         if (userConfirmed) {
-            // 사용자가 "예"를 선택한 경우 실행할 코드
             console.log("사용자가 예를 선택했습니다.");
-            // 여기서 캐릭터와의 기억과 편지 내용을 제거하는 코드를 추가합니다.
-            const response = await deleteLetters(characterId);
-
+            try {
+                await deleteLetters(characterId);
+                onReset();  // 상태 업데이트 함수 호출
+            } catch (error) {
+                console.error("Failed to delete letters:", error);
+            }
         } else {
-            // 사용자가 "아니오"를 선택한 경우 실행할 코드
             console.log("사용자가 아니오를 선택했습니다.");
         }
     }
--- src/pages/sendLetter/Sending.js
+++ src/pages/sendLetter/Sending.js
@@ -8,19 +8,23 @@ const Sending = () => {
   const { characterId } = location.state || {};
 
   useEffect(() => {
+    // characterId가 없으면 홈 페이지로 리다이렉트
+    if (!characterId) {
+      navigate('/');
+      return;
+    }
+
     const timer = setTimeout(() => {
-      navigate('/', { state: { characterId } }); // 이동할 경로로 변경
+      navigate('/', { state: { message: "편지 전송이 성공되었습니다. 답장이 오기까지 기다려주세요." } });
     }, 2000); // 2초 뒤에 이동
 
     return () => clearTimeout(timer);
   }, [navigate, characterId]);
 
   return (
-    <>
-      <div>
-        <img src="/images/sendLetter/send_letter.png" alt="편지" className="owl" />
-      </div>
-    </>
+    <div>
+      <img src="/images/sendLetter/send_letter.png" alt="편지" className="owl" />
+    </div>
   );
 };
 
